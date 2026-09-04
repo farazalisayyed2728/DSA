847562bc7c5d5e925c36e75cd99500c07dd0fff5
Sayyed Faraz Ali <farazalisayyed2728@gmail.com>
2026-09-04 19:14:35 +0530

    two pointer 2

--- two-pointer/2-pointer.py
+++ two-pointer/2-pointer.py
@@ -1,3 +1,17 @@
-#this is the 2 pointer file
- 
-print("this is the 2 pointer file")
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+    numbers = [2,7,11,15] , target = 9
+    a.sort()
+    i = 0 , j=n-1
+
+    while(i < j){
+        i = 0
+        int sum = a[i] + a[j]
+        if (sum == target)
+        return {i , j}
+    }
