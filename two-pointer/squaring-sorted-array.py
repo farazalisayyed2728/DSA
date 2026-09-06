be0f4950844909528fdbd7b10dbe780a6641c921
Sayyed Faraz Ali <farazalisayyed2728@gmail.com>
2026-09-06 11:14:49 +0530

    squaring sorted array 2.0

--- two-pointer/squaring-sorted-array.py
+++ two-pointer/squaring-sorted-array.py
@@ -1,18 +1,41 @@
 class Solution(object):
-    def sortedSquares(self, nums):
+    from typing import List
+    def sortedSquares(self, nums:List[int]) -> List [int]:
         """
         :type nums: List[int]
         :rtype: List[int]
         """
-        nums = [-4,-1,0,3,10]
-        i = 1
-        j = 1
-        n = len(nums)
+        siz = len(nums)
+        neg = []
+        pos = []
+       
+       for num in nums:
+        if num < 0:
+            neg.append(num)
+        else:
+            pos.append(num)
 
-        while i < j:
-            if nums[j] != nums[j - 1]:
-                nums[i] = nums[j]
+
+        #case 1
+        if len(neg) == 0:
+            return [X * X for X in pos]
+
+        #case 2
+        if len(pos) == 0:
+            res = [X * X for X in neg]
+            res.reverse()
+            return res
+
+        #case 3
+        neg = [X * X for X in neg][::-1] #sq , reverse
+        pos = [X * X for X in pos]
+        n , m = len(neg), len(pos)
+        res = []
+        
+        i = j = 0
+
+        while i < n and j < m :
+            if neg[i] <= pos[j]:
+                res.append(neg[i])
                 i += 1
-            j += 1
-
-        return nums
+                
