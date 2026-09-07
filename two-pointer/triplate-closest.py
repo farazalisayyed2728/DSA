36d2980766f3db4e5fa5db93438660610fcc9a8d
Sayyed Faraz Ali <farazalisayyed2728@gmail.com>
2026-09-07 11:07:10 +0530

    triplates run successfully

--- two-pointer/triplate-closest.py
+++ two-pointer/triplate-closest.py
@@ -1,39 +1,5 @@
 class Solution(object):
     def threeSumClosest(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: int
-        """
-        left = i+1
-        right = len(nums)-1
-
-        for i in range(len(nums) -2):
-            sum == target
-            left +=1
-            right -=1
-
-
-            while sum < target:
-                left += 1
-
-            while sum > target:
-                right -=1
-            left +=1
-            right -=1
-
-            elif sum < 0:
-                left += 1
-
-        else:
-            right -=1
-    return target
-                    
-
-
-
-class Solution(object):
-    def threeSumClosest(self, nums, target):
         nums.sort()
 
         closest = nums[0] + nums[1] + nums[2]
