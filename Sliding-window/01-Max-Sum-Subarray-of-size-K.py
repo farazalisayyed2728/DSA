c58948b3c2df3b51495795fc108d085743af80d4
Sayyed Faraz Ali <farazalisayyed2728@gmail.com>
2026-09-08 13:28:02 +0530

    sunarray of k

--- Sliding-window/01-Max-Sum-Subarray-of-size-K.py
+++ Sliding-window/01-Max-Sum-Subarray-of-size-K.py
@@ -9,7 +9,18 @@ class Solution:
             sum = sum + arr[i]
             if i <= high:
                 i += 1
+        while high < len(arr):
+            sum = sum -arr[low -1]
+            if high == len(arr):
+                break
+            res = max(res,sum)
+            low += 1
+            high += 1
+            
+            sum = sum + arr[high]
+            
+        return res
 
-                
+
        
             
