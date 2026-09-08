91e96048d2e0d4c7da6c3757be83b60e5162f64c
Sayyed Faraz Ali <farazalisayyed2728@gmail.com>
2026-09-08 19:17:11 +0530

    minimum size subarray sum

---
+++ Sliding-window/02-Minimum-Size-Subarray-Sum.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def minSubArrayLen(self, target, nums):
+        """
+        :type target: int
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        res = float('inf')
+        low = 0
+        high = 0
+        sum = 0
+
+        while high < len(nums):
+            sum += nums[high]
+
+            while sum >= target:
+
+                window_size = high - low + 1
+                res = min(res, high - low + 1)
+
+                sum -= nums[low]
+                low += 1
+
+            high += 1
+        if res == float('inf'):
+            return 0
+
+        
