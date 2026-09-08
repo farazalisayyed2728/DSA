92c6795f149532d15a92719a486b91d92bd06d10
Sayyed Faraz Ali <farazalisayyed2728@gmail.com>
2026-09-08 13:52:25 +0530

    sunarray of k successfully run

--- Sliding-window/01-Max-Sum-Subarray-of-size-K.py
+++ Sliding-window/01-Max-Sum-Subarray-of-size-K.py
@@ -1,25 +1,34 @@
 class Solution:
     def maxSubarraySum(self, arr, k):
-        # code here 
-        low = 0
-        high = k -1
+
+        n = len(arr)
         sum = 0
-        
+
         for i in range(k):
-            sum = sum + arr[i]
-            if i <= high:
-                i += 1
-        while high < len(arr):
-            sum = sum -arr[low -1]
-            if high == len(arr):
-                break
-            res = max(res,sum)
+            sum += arr[i]
+            i += 1
+
+            res = sum
+            low = 0
+            high = k -1
+
+        while high < n -1:
+            sum -= arr[low]
             low += 1
             high += 1
-            
-            sum = sum + arr[high]
-            
+        
+        
+            sum += arr[high]
+
+            res = max(res,sum)
         return res
+
+
+
+arr = [100,200,300,400 ]
+obj = Solution()
+result = obj.maxSubarraySum(arr, 2)
+print(result)
 
 
        
