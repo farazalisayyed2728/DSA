d44e50320a695994ae73ffb99578b694e760b9cd
Sayyed Faraz Ali <farazalisayyed2728@gmail.com>
2026-09-10 13:13:10 +0530

    characterReplacement

---
+++ Sliding-window/05-characterReplacement.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def characterReplacement(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        low = 0
+        high = 0
+        res = 0
+        f = {} #hashmap
+
+
+        while high < len(s):
+            f[s[high]] = f.get(s[high], 0)+1
+            high += 1
+
+
+            len_s = high - low
+            max_int = max(f.values())
+            diff = len_s - max_int
+
+            while diff > k:
+                f[s[low]] -=1
+                low +=1
+
+                max_int = max(f.values())
+                len_s = high - low
+                diff = len_s - max_int
+
+            len_s = high - low
+            res = max(res,len_s)
+
+        return res
