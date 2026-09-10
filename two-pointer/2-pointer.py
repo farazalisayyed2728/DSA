afbf6279ffc2f9749a024b6c785e3fc3b249b723
Sayyed Faraz Ali <farazalisayyed2728@gmail.com>
2026-09-10 19:12:56 +0530

    modified 2 sum

--- two-pointer/2-pointer.py
+++ two-pointer/2-pointer.py
@@ -1,31 +1,3 @@
-# class Solution(object):
-#         def twoSum(self, numbers, target):
-#             """
-#             :type numbers: List[int]
-#             :type target: int
-#             :rtype: List[int]
-#             """
-         
-
-#             left = 0
-#             right = len(numbers) -1
-            
-
-#             while left < right :
-#                 sum = numbers[left] + numbers[right]
-
-#                 if sum > target:
-#                     right -= 1
-                    
-
-#                 elif sum < target:
-#                     left += 1
-                   
-
-#                 else:
-#                     return [left + 1 , right + 1]
-
-
 class solution(object):
     def twosum(self,numbers,target):
         left  = 0
@@ -34,10 +6,16 @@ class solution(object):
         while left < right:
             sum  =  numbers[left] + numbers[right]
             if sum > target:
-                left -= 1
+                right -= 1
 
             elif sum< target:
                 right +=1
 
             else:
                 return[left +1, right + 1]
+
+
+numbers = [2, 7, 11, 15]
+solution = solution()
+result = solution.twosum(numbers, 9)
+print(result)
