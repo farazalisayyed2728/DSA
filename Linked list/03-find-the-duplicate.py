a1666e6da837328321e999f5869698658e3f4b76
Sayyed Faraz Ali <farazalisayyed2728@gmail.com>
2026-09-12 13:43:19 +0530

    find duplicate

---
+++ Linked list/03-find-the-duplicate.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        slow = 0
+        fast = 0
+
+        while True:
+            slow = nums[slow]
+            fast = nums[fast]
+            fast = nums[fast]
+            
+            if slow == fast:
+                slow =0
+
+                while fast != slow:
+                    slow = nums[slow]
+                    fast = nums[fast]
+                return fast
+
+        return 0
