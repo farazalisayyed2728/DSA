d9ed3c25b97dd019dff73aa70842623c516529d3
Sayyed Faraz Ali <farazalisayyed2728@gmail.com>
2026-09-16 12:34:42 +0530

    insert interval run

--- merge interval/02-insert-interval.py
+++ merge interval/02-insert-interval.py
@@ -6,11 +6,42 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         
-        intervals.sort()
         res = []
-        start1 = intervals[0][0]
-        end1 = intervals[0][1]
-        
+
+        start1 =  newInterval[0]
+        end1 =  newInterval[1]
+
+        insert = False
+
+        for i in range(len(intervals)):
+            start2 = intervals[i][0]
+            end2 = intervals[i][1]
+            
+
+            if end2 < start1:
+                res.append(intervals[i])
+                # start1 = start1
+                # end1  = max(end1 , end2)
+                continue
+            
+            if start2 > end1:
+                if insert == False:
+                    res.append([start1, end1])
+                    insert = True
+
+                res.append(intervals[i])
+                continue
+
+            
+            start1 = min(start1,start2)
+            end1 = max(end1 ,end2)
+
+
+        if insert == False:
+            res.append([start1 ,end1])
+
+        return res
+
 
 
 intervals = [[1,3],[6,9]]
