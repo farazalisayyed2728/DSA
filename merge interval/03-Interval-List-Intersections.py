20fdd7bf596e6bb21b5f3f91a03332d4e04ca03c
Sayyed Faraz Ali <farazalisayyed2728@gmail.com>
2026-09-16 22:03:36 +0530

    interval list intersection run

--- merge interval/03-Interval-List-Intersections.py
+++ merge interval/03-Interval-List-Intersections.py
@@ -39,3 +39,7 @@ class Solution(object):
 
         return res
 
+firstList = [[0,2],[5,10],[13,23],[24,25]]
+secondList = [[1,5],[8,12],[15,24],[25,26]]
+solution = Solution()
+print(solution.intervalIntersection(firstList, secondList))
