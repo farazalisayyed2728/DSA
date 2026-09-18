dc203e6bd4891d2ad3ac8cadb8f028b5c5f618ae
Sayyed Faraz Ali <farazalisayyed2728@gmail.com>
2026-09-18 12:02:31 +0530

    valid parentheses

---
+++ stack/03-valid-parentheses.py
@@ -0,0 +1,53 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        
+        st = []
+        
+
+        for i in range(len(s)):
+            
+            if s[i] == "(":
+                st.append(s[i])
+
+                continue
+
+            if s[i] == ")":
+                if not st or st[-1] != "(":
+                    return False
+                st.pop()
+                continue
+        
+
+
+            if s[i] == "{":
+                st.append(s[i])
+                continue 
+
+            if s[i] == "}":
+                if not st or st[-1] != "{":
+                    return False
+                st.pop()   
+                continue     
+                       
+            if s[i] == "[":
+                st.append(s[i])
+                continue 
+
+            if s[i] == "]" :
+                if not st or st[-1] != "[":
+
+                    return False
+                st.pop()
+                continue
+
+    
+
+
+        if st:
+            return False
+
+        return True
