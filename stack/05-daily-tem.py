04c84cbba359391029ac6bacfe7dc64726d62e02
Sayyed Faraz Ali <farazalisayyed2728@gmail.com>
2026-09-18 21:53:51 +0530

    daily temperatures

---
+++ stack/05-daily-tem.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        """
+        :type temperatures: List[int]
+        :rtype: List[int]
+        """
+        st =[]
+        res = [0] * len(temperatures)
+
+        for i in range(len(temperatures)):
+            
+
+            while st and  temperatures[st[-1]] <  temperatures[i]:
+                old = st.pop()
+                res[old] = i - old      
+            st.append( i)
+
+                
+        return res 
