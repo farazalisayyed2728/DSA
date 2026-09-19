3aed7d7be392506dd95419fb85aab9927fcc3746
Sayyed Faraz Ali <farazalisayyed2728@gmail.com>
2026-09-19 12:10:04 +0530

    Remove-All-Adjacent-Duplicates-in-String-II

---
+++ stack/06-Remove-All-Adjacent-Duplicates-in-String-II.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def removeDuplicates(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        
+        st = []
+        res = []
+
+        for i in range(len(s)):
+            c = s[i]
+
+            if not st:
+                st.append([c,1])
+                continue
+
+            if st[-1][0] != c:
+                st.append([c, 1])
+                continue
+
+            st[-1][1] += 1
+
+            if st[-1][1] == k :
+                st.pop()
+
+        while st:
+            p = st.pop()
+            
+            while p[1] > 0:
+                res.append(p[0])
+                p[1] -= 1
+
+        res.reverse()
+
+        return "".join(res)
