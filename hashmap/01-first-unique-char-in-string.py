b4c19aad1e793919fc5cc2c5a61f21a9975b2e37
Sayyed Faraz Ali <farazalisayyed2728@gmail.com>
2026-09-19 19:48:45 +0530

    first unique char in sti=ring

---
+++ hashmap/01-first-unique-char-in-string.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
+        f = {}
+
+        for i in range(len(s)):
+            f[s[i]] = f.get(s[i], 0) + 1
+
+
+        for i in range (len(s)):
+
+            if f[s[i]] == 1:
+                return i
+
+        return -1
