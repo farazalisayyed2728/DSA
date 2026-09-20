8b03d6f8bc4b397e071983cded4c37ca4a587593
Sayyed Faraz Ali <farazalisayyed2728@gmail.com>
2026-09-20 12:11:13 +0530

    longest palindrome

---
+++ hashmap/04-longest-palindrome.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        f = {}
+        for i in range(len(s)):
+            f[s[i]] = f.get(s[i], 0) + 1
+
+        odd = False
+        res = 0
+
+        for i in f:
+            val = f[i]
+
+            if val %2 == 0:
+                res += val
+
+            else:
+                res += val -1
+                odd = True
+
+
+        if odd:
+            return res + 1
+
+
+        return res
