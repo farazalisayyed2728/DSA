a55e96cce2581e6e94577a78dc2a7d7b552d17e6
Sayyed Faraz Ali <farazalisayyed2728@gmail.com>
2026-09-25 19:12:17 +0530

    koko eating bananas

---
+++ binary-search/04-koko-eating-bananas.py
@@ -0,0 +1,32 @@
+class Solution(object):
+        
+    def fun(self , piles , speed):
+        h = 0
+
+        for i in range(len(piles)):
+            h += piles[i] // speed
+
+            if piles[i] % speed != 0:
+                h += 1
+
+        return h
+        
+    def minEatingSpeed(self, piles, h):
+        
+        low = 1
+        high = max(piles)
+        res = high
+
+        while low <= high:
+            guess = low + (high - low) // 2
+
+            hour = self.fun(piles, guess)
+
+            if hour > h:
+                low = guess + 1
+
+            else:
+                res = guess
+                high = guess - 1
+
+        return res
